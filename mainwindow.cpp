af4957901f7c42e5988d439e10617b0a6d116f79
myshkaras <family49714@gmail.com>
2019-06-16 20:04:46 +0300

    Release

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,5 +1,6 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include "readsavefile.h"
 
 #include <QAction>
 #include <QApplication>
@@ -42,349 +43,305 @@
 
 const QString rsrcPath = ":/images";
 
-MainWindow::MainWindow(QWidget *parent) :
+MainWindow::MainWindow(QWidget* parent) :
     QMainWindow(parent),
-    ui(new Ui::MainWindow){
-
-    ui->setupUi(this);
+    _ui(new Ui::MainWindow)
+{
+    _ui->setupUi(this);
 
     //connect this action to the slot open()
-    connect(ui->actOpen, SIGNAL(triggered()), this, SLOT(Open()));
-    connect(ui->actSave, SIGNAL(triggered()), this, SLOT(Save()));
-    connect(ui->actSaveAs, SIGNAL(triggered()), this, SLOT(SaveAs()));
-    connect(ui->actNew, SIGNAL(triggered()), this, SLOT(New()));
-    connect(ui->actTextFont, SIGNAL(triggered()), this, SLOT(SetTextFont()));
-    connect(ui->actTextColor, SIGNAL(triggered()), this, SLOT(SetTextColor()));
-    connect(ui->actTextBackgroundColor, SIGNAL(triggered()), this, SLOT(SetTextBackgroundColor()));
-    connect(ui->actBackgroundColor, SIGNAL(triggered()), this, SLOT(SetBackgroundColor()));
-    connect(ui->actionList, SIGNAL(triggered()), this, SLOT(ListAvailableCodecs()));
-    connect(ui->actionUTF_8, SIGNAL(triggered()), this, SLOT(SetActionUTF_8()));
-    connect(ui->actionUTF_16, SIGNAL(triggered()), this, SLOT(SetActionUTF_16()));
-    connect(ui->actionWin_1251, SIGNAL(triggered()), this, SLOT(SetActionWin_1251()));
-    connect(ui->actionKOI_8R, SIGNAL(triggered()), this, SLOT(SetActionKOI_8R()));
+    connect(_ui->actOpen, SIGNAL(triggered()), this, SLOT(open()));
+    connect(_ui->actSave, SIGNAL(triggered()), this, SLOT(save()));
+    connect(_ui->actSaveAs, SIGNAL(triggered()), this, SLOT(saveAs()));
+    connect(_ui->actNew, SIGNAL(triggered()), this, SLOT(New()));
+    connect(_ui->actTextFont, SIGNAL(triggered()), this, SLOT(setTextFont()));
+    connect(_ui->actTextColor, SIGNAL(triggered()), this, SLOT(setTextColor()));
+    connect(_ui->actTextBackgroundColor, SIGNAL(triggered()), this, SLOT(setTextBackgroundColor()));
+    connect(_ui->actBackgroundColor, SIGNAL(triggered()), this, SLOT(setBackgroundColor()));
+    connect(_ui->actionUTF_8, SIGNAL(triggered()), this, SLOT(setActionUTF_8()));
+    connect(_ui->actionUTF_16, SIGNAL(triggered()), this, SLOT(setActioncp866()));
+    connect(_ui->actionWin_1251, SIGNAL(triggered()), this, SLOT(setActionWin_1251()));
+    connect(_ui->actionKOI_8R, SIGNAL(triggered()), this, SLOT(setActionKOI_8R()));
 
-    ui->actionUTF_8->setCheckable(true);
-    ui->actionUTF_16->setCheckable(true);
-    ui->actionWin_1251->setCheckable(true);
-    ui->actionKOI_8R->setCheckable(true);
+    _ui->actionUTF_8->setCheckable(true);
+    _ui->actionUTF_16->setCheckable(true);
+    _ui->actionWin_1251->setCheckable(true);
+    _ui->actionKOI_8R->setCheckable(true);
 
-    EncodingGroup = new QActionGroup(this);
-    EncodingGroup->addAction(ui->actionUTF_8);
-    EncodingGroup->addAction(ui->actionUTF_16);
-    EncodingGroup->addAction(ui->actionWin_1251);
-    EncodingGroup->addAction(ui->actionKOI_8R);
-    ui->actionUTF_8->setChecked(true);
+    _encodingGroup = new QActionGroup(this);
+    _encodingGroup->addAction(_ui->actionUTF_8);
+    _encodingGroup->addAction(_ui->actionUTF_16);
+    _encodingGroup->addAction(_ui->actionWin_1251);
+    _encodingGroup->addAction(_ui->actionKOI_8R);
+    _ui->actionUTF_8->setChecked(true);
 
-    codec = QTextCodec::codecForName("UTF-8");
+    _edFile.setCodec("UTF-8");
 
     QPixmap pix(16, 16);
     pix.fill(Qt::black);
-    ui->actTextColor->setIcon(pix);
+    _ui->actTextColor->setIcon(pix);
 
     QPixmap bgpix(16, 16);
     bgpix.fill(Qt::white);
-    ui->actBackgroundColor->setIcon(bgpix);
+    _ui->actBackgroundColor->setIcon(bgpix);
 
-    ui->teLines->setWordWrapMode(QTextOption::NoWrap);
-
-}
-
-MainWindow::~MainWindow(){
-
-    delete ui;
+    _ui->teLines->setWordWrapMode(QTextOption::NoWrap);
 
 }
 
-void MainWindow::SetCurrentFileName(const QString &fileName){
+MainWindow::~MainWindow()
+{
+    delete _ui;
+    delete _tb;
+    delete _encodingGroup;
+}
 
-    this->fileName = fileName;
-    ui->teLines->document()->setModified(false);
+void MainWindow::setCurrentFileName(const QString &fileName)
+{
+    _ui->teLines->document()->setModified(false);
 
     QString shownName;
     if (fileName.isEmpty())
-        shownName = "untitled.txt";
-    else
+    {
+        shownName = "untitled.txt";    
+        _edFile.setFileName(shownName);
+    }else
+    {
         shownName = QFileInfo(fileName).fileName();
+        _edFile.setFileName(fileName);
+    }
 
     setWindowTitle(tr("%1[*] - %2").arg(shownName, QCoreApplication::applicationName()));
     setWindowModified(false);
-
 }
 
-bool MainWindow::Load(const QString &f){
+bool MainWindow::load(const QString &f)
+{
+    if (_edFile.exists(f))
+    {
+        setCurrentFileName(f);
+        _ui->teLines->setPlainText(_edFile.loadFileTo(f));
 
-    if (!QFile::exists(f)){
-
+        return true;
+    }else
+    {
         return false;
-
     }
-
-    QFile file(f);
-
-    if (!file.open(QFile::ReadOnly)){
-
-        return false;
-
-    }
-
-    QTextStream stream (&file);
-     stream.setCodec(codec);
-
-     QString str =  stream.readAll();
-
-     ui->teLines->setPlainText(str);
-
-    SetCurrentFileName(f);
-
-    return true;
-
 }
 
-void MainWindow::Open(){
-
+void MainWindow::open()
+{
     QFileDialog fileDialog(this, tr("Open File..."));
     fileDialog.setAcceptMode(QFileDialog::AcceptOpen);
     fileDialog.setFileMode(QFileDialog::ExistingFile);
     fileDialog.setNameFilter(tr("CPP(*.cpp *.h);;Text files (*.txt);;Python files (*.py)"));
 
-    if (fileDialog.exec() != QDialog::Accepted){
-
+    if (fileDialog.exec() != QDialog::Accepted)
+    {
         return;
-
     }
+
     const QString fn = fileDialog.selectedFiles().first();
-    if (Load(fn)){
 
+    if (load(fn))
+    {
         statusBar()->showMessage(tr("File Opened \"%1\"").arg(QDir::toNativeSeparators(fn)));
-
-    }else{
-
+    }else
+    {
         statusBar()->showMessage(tr("Could Not Open File\"%1\"").arg(QDir::toNativeSeparators(fn)));
-
     }
 }
 
-bool MainWindow::Save(){
-
-    if (fileName.isEmpty())
+bool MainWindow::save()
+{
+    if (_edFile.getFileName().isEmpty())
     {
-
-        return SaveAs();
-
-    }
-    if (fileName.startsWith(QStringLiteral(":/")))
-    {
-
-        return SaveAs();
-
+        return saveAs();
     }
 
-    QTextDocumentWriter writer(fileName);
-    writer.setCodec(codec);
-    bool success = writer.write(ui->teLines->document());
-    if (success) {
+    if (_edFile.getFileName().startsWith(QStringLiteral(":/")))
+    {
+        return saveAs();
+    }
 
-        ui->teLines->document()->setModified(false);
-        statusBar()->showMessage(tr("File Saved \"%1\"").arg(QDir::toNativeSeparators(fileName)));
+    bool success = _edFile.saveFile(_ui->teLines->document());
 
-    } else {
-
+    if (success)
+    {
+        _ui->teLines->document()->setModified(false);
+        statusBar()->showMessage(tr("File Saved \"%1\"").arg(QDir::toNativeSeparators(_edFile.getFileName())));
+    } else
+    {
         statusBar()->showMessage(tr("Could Not Save The File \"%1\"")
-                                 .arg(QDir::toNativeSeparators(fileName)));
-
+                                 .arg(QDir::toNativeSeparators(_edFile.getFileName())));
     }
 
     return success;
-
 }
 
-bool MainWindow::SaveAs(){
-
+bool MainWindow::saveAs()
+{
     QFileDialog fileDialog(this, tr("Save as..."));
     fileDialog.setAcceptMode(QFileDialog::AcceptSave);
     fileDialog.setNameFilter(tr("CPP(*.cpp *.h);;Text files (*.txt);;Python files (*.py)"));
     fileDialog.setDefaultSuffix("txt");
     if (fileDialog.exec() != QDialog::Accepted)
+    {
         return false;
+    }
     const QString fn = fileDialog.selectedFiles().first();
-    SetCurrentFileName(fn);
+    setCurrentFileName(fn);
 
-    return Save();
-
+    return save();
 }
 
 
-bool MainWindow::SaveNeeded(){
-
-    if (!ui->teLines->document()->isModified()){
-
+bool MainWindow::saveNeeded()
+{
+    if (!_ui->teLines->document()->isModified())
+    {
         return true;
-
     }
 
     const QMessageBox::StandardButton resBtn =
-        QMessageBox::warning(this, QCoreApplication::applicationName(),
-                             tr("The document was changed.\n"
-                                "Do you want to save it?"),
-                             QMessageBox::Save | QMessageBox::Discard | QMessageBox::Cancel);
+        QMessageBox::warning(this, QCoreApplication::applicationName(),tr("The document was changed.\n"
+                        "Do you want to save it?"),QMessageBox::Save | QMessageBox::Discard | QMessageBox::Cancel);
+
     if (resBtn == QMessageBox::Save)
     {
-
-        return Save();
-
+        return save();
     }else
-        if (resBtn == QMessageBox::Cancel){
-
+    {
+        if (resBtn == QMessageBox::Cancel)
+        {
             return false;
         }
+    }
 
     return true;
-
 }
 
-
-
-void MainWindow::New(){
-
-    if (SaveNeeded()){
-
-        ui->teLines->clear();
-        SetCurrentFileName(QString());
-
+void MainWindow::New()
+{
+    if (saveNeeded())
+    {
+        _ui->teLines->clear();
+        setCurrentFileName(QString());
     }
 
     statusBar()->showMessage(tr("New File Created"),2000);
-
 }
 
-void MainWindow::SetTextFont(){
-
+void MainWindow::setTextFont()
+{
     bool ok;
-    QFont font = QFontDialog::getFont(
-                    &ok, QFont("Courier 10 Pitch Regular", 10), this);
-
+    QFont font = QFontDialog::getFont(&ok, QFont("Courier 10 Pitch Regular", 10), this);
     QTextCharFormat fmt;
     fmt.setFont(font);
-    ChangeSelectionFormat(fmt);
-
+    changeSelectionFormat(fmt);
     statusBar()->showMessage(font.family(),2000);
-
 }
 
-void MainWindow::ChangeSelectionFormat(const QTextCharFormat &format){
+void MainWindow::changeSelectionFormat(const QTextCharFormat &format)
+{
+    QTextCursor cursor = _ui->teLines->textCursor();
 
-    QTextCursor cursor = ui->teLines->textCursor();
-
-    if (!cursor.hasSelection()){
-
+    if (!cursor.hasSelection())
+    {
         cursor.select(QTextCursor::WordUnderCursor);
-
     }
+
     cursor.mergeCharFormat(format);
-    ui->teLines->mergeCurrentCharFormat(format);
-
+    _ui->teLines->mergeCurrentCharFormat(format);
 }
 
-void MainWindow::TextColorChanged(const QColor &c){
-
-    QPixmap pix(16, 16);
-    pix.fill(c);
-    ui->actTextColor->setIcon(pix);
-
-}
-
-void MainWindow::SetTextColor(){
-
-    QColor col = QColorDialog::getColor(ui->teLines->textColor(), this);
-    if (!col.isValid())
-        return;
-    QTextCharFormat fmt;
-    fmt.setForeground(col);
-    ChangeSelectionFormat(fmt);
-    TextColorChanged(col);
-
-    statusBar()->showMessage(tr("Text Color changed"),2000);
-
-}
-
-void MainWindow::TextBackgroundColorChanged(const QColor &c)
+void MainWindow::textColorChanged(const QColor &c)
 {
     QPixmap pix(16, 16);
     pix.fill(c);
-    ui->actTextBackgroundColor->setIcon(pix);
+    _ui->actTextColor->setIcon(pix);
 }
 
-void MainWindow::SetTextBackgroundColor(){
-
-    QColor col = QColorDialog::getColor(ui->teLines->textColor(), this);
+void MainWindow::setTextColor()
+{
+    QColor col = QColorDialog::getColor(_ui->teLines->textColor(), this);
     if (!col.isValid())
+    {
         return;
-    QTextCharFormat fmt;
-    fmt.setBackground(col);
-    ChangeSelectionFormat(fmt);
-    TextBackgroundColorChanged(col);
-
-    statusBar()->showMessage(tr("Text Background Color changed"),2000);
-
-}
-
-void MainWindow::BackgroundColorChanged(const QColor &c){
-
-    QPixmap pix(16, 16);
-    pix.fill(c);
-    ui->actBackgroundColor->setIcon(pix);
-
-}
-
-void MainWindow::SetBackgroundColor(){
-
-    QColor col = QColorDialog::getColor(ui->teLines->textColor(), this);
-    if (!col.isValid()){
-
-        return;
-
     }
 
-    QPalette p = ui->teLines->palette();
+    QTextCharFormat fmt;
+    fmt.setForeground(col);
+    changeSelectionFormat(fmt);
+    textColorChanged(col);
+
+    statusBar()->showMessage(tr("Text Color changed"),2000);
+}
+
+void MainWindow::textBackgroundColorChanged(const QColor &c)
+{
+    QPixmap pix(16, 16);
+    pix.fill(c);
+    _ui->actTextBackgroundColor->setIcon(pix);
+}
+
+void MainWindow::setTextBackgroundColor(){
+
+    QColor col = QColorDialog::getColor(_ui->teLines->textColor(), this);
+    if (!col.isValid())
+    {
+        return;
+    }
+
+    QTextCharFormat fmt;
+    fmt.setBackground(col);
+    changeSelectionFormat(fmt);
+    textBackgroundColorChanged(col);
+
+    statusBar()->showMessage(tr("Text Background Color changed"),2000);
+}
+
+void MainWindow::backgroundColorChanged(const QColor &c)
+{
+    QPixmap pix(16, 16);
+    pix.fill(c);
+    _ui->actBackgroundColor->setIcon(pix);
+}
+
+void MainWindow::setBackgroundColor()
+{
+    QColor col = QColorDialog::getColor(_ui->teLines->textColor(), this);
+    if (!col.isValid())
+    {
+        return;
+    }
+
+    QPalette p = _ui->teLines->palette();
     p.setColor(QPalette::Base, col);
-    ui->teLines->setPalette(p);
-    BackgroundColorChanged(col);
+    _ui->teLines->setPalette(p);
+    backgroundColorChanged(col);
 
     statusBar()->showMessage(tr("Background Color changed"),2000);
-
 }
 
-void MainWindow::ListAvailableCodecs(){
-
-    QList<QByteArray>  QL=QTextCodec::availableCodecs();
-    QListIterator<QByteArray> i(QL);
-    while (i.hasNext())
-    ui->teLines->append(i.next());
-
+void MainWindow::setActionUTF_8()
+{
+    _edFile.setCodec("UTF-8");
 }
 
-void MainWindow::SetActionUTF_8(){
-
-    codec = QTextCodec::codecForName("UTF-8");
-
+void MainWindow::setActioncp866()
+{
+    _edFile.setCodec("cp866");
 }
 
-void MainWindow::SetActionUTF_16(){
-
-    codec = QTextCodec::codecForName("UTF-16");
-
+void MainWindow::setActionWin_1251()
+{
+    _edFile.setCodec("windows-1251");
 }
 
-void MainWindow::SetActionWin_1251(){
-
-    codec = QTextCodec::codecForName("windows-1251");
-
-}
-
-void MainWindow::SetActionKOI_8R(){
-
-    codec = QTextCodec::codecForName("KOI8-R");
-
+void MainWindow::setActionKOI_8R()
+{
+    _edFile.setCodec("KOI8-R");
 }
 
 
